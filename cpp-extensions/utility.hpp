c499a781974ce0ea38082b663dd69c789f582b4e
Михайлов Антон <anion155@gmail.com>
2018-03-04 16:56:03 +0500

    Utility-classes:
    + UNUSED sink
    + StringStreamer wrapper

---
+++ cpp-extensions/utility.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "_defines.hpp"
+#include <sstream>
+#include <utility>
+
+CE_NAMESPACE_BEGIN
+
+template <typename ...Args>
+void UNUSED(Args&& ...args) {
+    (void)(sizeof...(args));
+}
+
+class ss {
+  std::stringstream m_stream;
+public:
+  operator std::string() {
+    return m_stream.str();
+  }
+
+  template<typename T>
+  ss &operator<<(const T &op) {
+    m_stream << op;
+    return *this;
+  }
+};
+CE_NAMESPACE_END
